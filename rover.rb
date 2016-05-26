ead2c79b406603d1172c347c1820f894680b6aa5
Auke van Scheltinga <aukevs@gmail.com>
2016-05-26 11:09:55 +0200

    improve edge detection test catch and fix bug

--- rover.rb
+++ rover.rb
@@ -54,9 +54,9 @@ class Rover
 
 	def move_impossible?
 		case @direction
-		when "N" || "E"
-			(@x + 1 > AREA_LIMIT[0]) || (@y + 1 > AREA_LIMIT[1])
-		when "W" || "S"
+		when "N", "E"
+		  (@x + 1 > AREA_LIMIT[0]) || (@y + 1 > AREA_LIMIT[1])
+		when "W", "S"
 			(@x - 1 < 0) || (@y - 1 < 0)
 		end		
 	end
@@ -118,17 +118,35 @@ class TestSuite < MiniTest::Test
 		assert rover.direction = "E"
 	end
 
-	def test_plateau_edge_crash_protection
+	def test_crash_protection_top
 		rover = Rover.new(5,5,"N")
 		rover.navigate("M")
 		assert rover.x == 5
 		assert rover.y == 5
 		assert rover.direction = "N"
+	end
 
-		rover = Rover.new(0,0,"W")
+	def test_crash_protection_right
+		rover = Rover.new(5,1,"E")
+		rover.navigate("M")
+		assert rover.x == 5
+		assert rover.y == 1
+		assert rover.direction = "E"
+	end
+
+	def test_crash_protection_bottom
+		rover = Rover.new(2,0,"S")
+		rover.navigate("M")
+		assert rover.x == 2
+		assert rover.y == 0
+		assert rover.direction = "S"
+	end
+
+	def test_crash_protection_left
+		rover = Rover.new(0,3,"W")
 		rover.navigate("M")
 		assert rover.x == 0
-		assert rover.y == 0
+		assert rover.y == 3
 		assert rover.direction = "W"
 	end
 
